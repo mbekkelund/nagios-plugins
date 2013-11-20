836f26adc3abfbec4f94c6e0c6f78d694cdb1a94
Morten Bekkelund <morten.bekkelund@met.no>
2013-11-20 11:09:19 +0100

    evul \t

--- check_by_salt.py
+++ check_by_salt.py
@@ -29,33 +29,33 @@ NAGIOS_EXIT = {"OK": 0, "WARNING": 1, "CRITICAL": 2, "UNKNOWN": 3}
 client = salt.client.LocalClient()
 
 def execute_plugin(host,plugin,timeout):
-	""" 
-		executing nagiosplugin via salt's cmd.run_all function
-	"""
+    """ 
+        executing nagiosplugin via salt's cmd.run_all function
+    """
 
-	ret 	= client.cmd(host, "cmd.run_all", [plugin], timeout=timeout)
-	exit 	= ret[host]["retcode"]
-	output 	= ret[host]["stdout"]
+    ret    = client.cmd(host, "cmd.run_all", [plugin], timeout=timeout)
+    exit   = ret[host]["retcode"]
+    output = ret[host]["stdout"]
 
-	return exit, output
+    return exit, output
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Options:')
-	parser.add_argument('-H', metavar='host', type=str, help='remote host to be queried', required=True)
-	parser.add_argument('-p', metavar='plugin', type=str, help='full path to remote nagiosplugin', required=True)
-	parser.add_argument('-t', metavar='timeout', type=str, help='timout in seconds', required=False)
+    parser = argparse.ArgumentParser(description='Options:')
+    parser.add_argument('-H', metavar='host', type=str, help='remote host to be queried', required=True)
+    parser.add_argument('-p', metavar='plugin', type=str, help='full path to remote nagiosplugin', required=True)
+    parser.add_argument('-t', metavar='timeout', type=str, help='timout in seconds', required=False)
 
-	try:
-		arguments = parser.parse_args()
-	except Exception as e:
-		print "Error in argumentparsing: {0}".format(e)
-		sys.exit(NAGIOS_EXIT["UNKNOWN"])
+    try:
+        arguments = parser.parse_args()
+    except Exception as e:
+        print "Error in argumentparsing: {0}".format(e)
+        sys.exit(NAGIOS_EXIT["UNKNOWN"])
 
-	(exit, output) = execute_plugin(arguments.H,arguments.p,arguments.t)
-	if not output:
-		print "Remote execution of {0} unsuccessful.".format(arguments.p)
-		print "Check if the plugin exists on the remote host and that the permissions" \
-				"are correct."
+    (exit, output) = execute_plugin(arguments.H,arguments.p,arguments.t)
+    if not output:
+        print "Remote execution of {0} unsuccessful.".format(arguments.p)
+        print "Check if the plugin exists on the remote host and that the permissions" \
+                "are correct."
 
 print(output)
 sys.exit(exit)
