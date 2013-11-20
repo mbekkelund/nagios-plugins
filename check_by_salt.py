f726dbdcd0eed808f0a38df9726fa15d4d168cc5
Morten Bekkelund <morten.bekkelund@met.no>
2013-11-20 11:08:18 +0100

    added some gplstuffs

--- check_by_salt.py
+++ check_by_salt.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (C) 2013 Morten Bekkelund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# See: http://www.gnu.org/copyleft/gpl.html
 #
 # The intention of this script is to replace NRPE, ssh etc for remote execution
 # of nagiosplugins. The script runs as root on the salt-master.
